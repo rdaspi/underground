9bd002ecc662a7cf3ccca700fa75df27c43a0ef9
rayne <rybak_daria@ukr.net>
2021-03-22 23:08:17 +0200

    new view for all message

--- java/src/Universe.java
+++ java/src/Universe.java
@@ -1,9 +1,10 @@
 public class Universe {
     public static void main(String[] args) {
         String chocolateName = "Millenium";
+        String alcohol = "Vodka";
+
         System.out.println("I need a chocolate " + chocolateName + ".");
         System.out.println("After that I need a tea.");
-        String alcohol = "Vodka";
         System.out.println("I need a cocktail with " + alcohol);
     }
 }
