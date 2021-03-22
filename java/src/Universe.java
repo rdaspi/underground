501197ae913a18844f08e348491f0e69e8f9f3dd
rayne <rybak_daria@ukr.net>
2021-03-22 22:06:28 +0200

    OnePointAddedToWishList

---
+++ java/src/Universe.java
@@ -0,0 +1,6 @@
+public class Universe {
+    public static void main(String[] args) {
+        String chocolateName = "Milka";
+        System.out.println("I need a chocolate " + chocolateName);
+    }
+}
