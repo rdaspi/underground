80c9ce72212fdcd536a5a5ee2b13a2828bf334b6
rayne <rybak_daria@ukr.net>
2021-03-22 23:17:48 +0200

    cake recipe

--- java/src/Universe.java
+++ java/src/Universe.java
@@ -1,9 +1,14 @@
 public class Universe {
     public static void main(String[] args) {
-        String chocolateName = "Millenium";
-        System.out.println("I need a chocolate " + chocolateName + ".");
-        System.out.println("After that I need a tea.");
-        String alcohol = "Vodka";
-        System.out.println("I need a cocktail with " + alcohol);
+        String ingredient1 = "Milk";
+        String ingredient2 = "Sugar";
+        String ingredient3 = "Schaer";
+        String ingredient4 = "Cream";
+        System.out.println("To make a cake for the celebration we need to take all ingredients:");
+        System.out.println("1. " + ingredient1);
+        System.out.println("2. " + ingredient2);
+        System.out.println("3. " + ingredient3);
+        System.out.println("4. " + ingredient4);
+        System.out.println("And mix them all together.");
     }
 }
